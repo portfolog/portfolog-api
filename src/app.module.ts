ce37041b7fc98c3f56c8a14d9e45bca88d57c3d7
vietantran1215 <vietantran1215@gmail.com>
2025-01-09 13:56:32 +0700

    Generating models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { ProjectModule } from './project/project.module';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PortfolioModule, ProjectModule, ArticleModule],
   controllers: [AppController],
   providers: [AppService],
 })
